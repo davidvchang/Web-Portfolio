f39375b8e6ca40e50d3acbd6f8854dcfd7a5b809
davidvchang <dvalenzuelachang@gmail.com>
2025-05-06 22:25:02 -0700

    fix: change rounded in Skills container

--- src/components/SkillsContainer.jsx
+++ src/components/SkillsContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SkillsContainer = ({text, children}) => {
   return (
-    <div className='flex flex-col bg-white/5 backdrop-blur-xl p-5 gap-5 '>
+    <div className='flex flex-col bg-white/5 backdrop-blur-xl p-5 gap-5 rounded-lg'>
         <span className='text-xl font-medium text-center'>{text}</span>
 
         <div className='grid grid-cols-3 gap-3'>
