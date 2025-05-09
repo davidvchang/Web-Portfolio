e21c1996c85339a1c025da28d9b334002bc474b2
davidvchang <dvalenzuelachang@gmail.com>
2025-05-08 23:43:47 -0700

    Add: projects

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -5,6 +5,9 @@ import TechnologyUsed from '../components/TechnologyUsed'
 import { Github } from 'lucide-react'
 
 import Joinly from '../assets/img/covers/Joinly/Home.webp'
+import BlogSpace from '../assets/img/covers/BlogSpace/BlogSpace.webp'
+import LibrarySystem from '../assets/img/covers/Library-System/Library-System.webp'
+import Ecommerce from '../assets/img/covers/Ecommerce/Ecommerce.webp'
 
 const Projects = () => {
   return (
@@ -27,6 +30,66 @@ const Projects = () => {
                 children2={
                     <>
                         <BtnActionsProject link="https://github.com/davidvchang/Joinly" icon={<Github className='text-white w-5 h-5'/>} text="Ver repositorio"/>
+                        {/* <BtnActionsProjects link="https://pelis-dev.netlify.app/" icon={<Link className='text-white w-5 h-5'/>} text="Live Demo"/> */}
+                    </>
+                }
+            />
+
+            <ProjectCard image={BlogSpace} title="BlogSpace" description="Página en la cuál puedes ver todos los blogs que los usuarios han agregado, como también puedes registrarte, iniciar sesión y poder crear tus propio blogs, eliminar o editar los blogs ya creados y comentar otros blogs." 
+                
+                children={
+                    <>
+                        <TechnologyUsed name="React"/>
+                        <TechnologyUsed name="TypeScript"/>
+                        <TechnologyUsed name="Tailwind"/>
+                        <TechnologyUsed name="Express.js"/>
+                        <TechnologyUsed name="PostgreSQL"/>
+                    </>
+                }
+
+                children2={
+                    <>
+                        <BtnActionsProject link="https://github.com/davidvchang/BlogSpace" icon={<Github className='text-white w-5 h-5'/>} text="Ver repositorio"/>
+                        {/* <BtnActionsProjects link="https://pelis-dev.netlify.app/" icon={<Link className='text-white w-5 h-5'/>} text="Live Demo"/> */}
+                    </>
+                }
+            />
+
+            <ProjectCard image={LibrarySystem} title="Sistema para Biblioteca" description="Sistema fullstack para una biblioteca en la cual permita prestar libros, así como agregar nuevos libros, poder editar, eliminar cada libro, registrar a clientes, etc." 
+                
+                children={
+                    <>
+                        <TechnologyUsed name="React"/>
+                        <TechnologyUsed name="TypeScript"/>
+                        <TechnologyUsed name="Tailwind"/>
+                        <TechnologyUsed name="Express.js"/>
+                        <TechnologyUsed name="PostgreSQL"/>
+                    </>
+                }
+
+                children2={
+                    <>
+                        <BtnActionsProject link="https://github.com/davidvchang/Sistema-para-biblioteca" icon={<Github className='text-white w-5 h-5'/>} text="Ver repositorio"/>
+                        {/* <BtnActionsProjects link="https://pelis-dev.netlify.app/" icon={<Link className='text-white w-5 h-5'/>} text="Live Demo"/> */}
+                    </>
+                }
+            />
+
+            <ProjectCard image={Ecommerce} title="TechQuality Ecommerce" description="Ecommerce para una tienda ficticia de tecnologia. Permite registrarse, iniciar sesón, agregar productos al carrito y comprar por medio de mercado pago." 
+                
+                children={
+                    <>
+                        <TechnologyUsed name="React"/>
+                        <TechnologyUsed name="TypeScript"/>
+                        <TechnologyUsed name="Tailwind"/>
+                        <TechnologyUsed name="Express.js"/>
+                        <TechnologyUsed name="PostgreSQL"/>
+                    </>
+                }
+
+                children2={
+                    <>
+                        <BtnActionsProject link="https://github.com/davidvchang/TechQuality---Ecommerce" icon={<Github className='text-white w-5 h-5'/>} text="Ver repositorio"/>
                         {/* <BtnActionsProjects link="https://pelis-dev.netlify.app/" icon={<Link className='text-white w-5 h-5'/>} text="Live Demo"/> */}
                     </>
                 }
