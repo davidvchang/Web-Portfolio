53341997e6efd140006b6c47d79b0f93a48f1989
davidvchang <dvalenzuelachang@gmail.com>
2025-05-08 23:03:09 -0700

    Fix: change bg colo btnaction in projects

--- src/components/BtnActionsProject.jsx
+++ src/components/BtnActionsProject.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BtnActionsProject = ({link, icon, text}) => {
   return (
-    <a href={link} target='_blank' className='flex gap-2 items-center text-sm w-fit h-fit px-3 py-2 rounded-full text-white border border-emerald-900 bg-emerald-950 hover:bg-teal-700 hover:transition duration-300 font-light'>
+    <a href={link} target='_blank' className='flex gap-2 items-center text-sm w-fit h-fit px-3 py-2 rounded-full text-white border border-emerald-900 bg-emerald-950 hover:bg-green-800 hover:transition duration-300 font-light'>
         {icon}
         <span>{text}</span>
     </a>
